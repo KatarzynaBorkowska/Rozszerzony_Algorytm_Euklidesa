5509b2dbd14514ff54e9601b9af22190853cb1b1
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-05-29 21:53:18 +0200

    Initial Commit

---
+++ Rozszerzony_Algorytm_Euklidesa/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Rozszerzony_Algorytm_Euklidesa
+//
+//  Created by Kasia on 29/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
